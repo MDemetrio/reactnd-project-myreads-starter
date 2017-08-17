5333cc74333195c76ac05cfec805cdd9396f2745
Matheus Demetrio <math2706@hotmail.com>
2017-08-17 07:45:44 -0300

    feat: Add offline capabilities to the BookShelves Component

--- src/BookShelves.js
+++ src/BookShelves.js
@@ -44,14 +44,24 @@ class BookShelves extends Component {
     state = {
         books: []
     }
-    componentDidMount(){
-        BooksAPI.getAll().then((books => this.setState({books})))
+
+    componentWillMount() {
+        const books = window.localStorage.getItem('books') || '[]';
+        this.setState({ books: JSON.parse(books) });
     }
+
+    componentDidMount() {
+        BooksAPI.getAll().then((books => {
+            this.setState({ books });
+            window.localStorage.setItem('books', JSON.stringify(books));
+        }))
+    }
+
     render() {
         const wantToReadBooks = this.state.books.filter(x => x.shelf === 'wantToRead');
         const currentlyReadingBooks = this.state.books.filter(x => x.shelf === 'currentlyReading');
         const readBooks = this.state.books.filter(x => x.shelf === 'read');
-        
+
         return (
             <div className="list-books">
                 <div className="list-books-title">
@@ -60,8 +70,8 @@ class BookShelves extends Component {
                 <div className="list-books-content">
                     <div>
                         <BookShelf name="Want to Read" books={wantToReadBooks} />
-                        <BookShelf name="Read" books={currentlyReadingBooks}/>
-                        <BookShelf name="Reading" books={readBooks}/>
+                        <BookShelf name="Read" books={currentlyReadingBooks} />
+                        <BookShelf name="Reading" books={readBooks} />
                     </div>
                 </div>
                 <div className="open-search">
