774b5a00ab65d3929ce6c65ab00a2966e8ea735c
Matheus Demetrio <math2706@hotmail.com>
2017-08-17 07:38:43 -0300

    feat: Separate Books and Shelfs into components and fetch books from API

--- src/BookShelves.js
+++ src/BookShelves.js
@@ -1,8 +1,57 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+
+const BookShelf = ({ name, books }) => {
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{name}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {books.map(book =>
+                        <li key={book.id}>
+                            <Book coverUrl={book.imageLinks.thumbnail} name={book.title} authors={book.authors} />
+                        </li>
+                    )}
+                </ol>
+            </div>
+        </div>
+    )
+}
+
+const Book = ({ coverUrl, title, authors }) => {
+    return (
+        <div className="book">
+            <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${coverUrl})` }}></div>
+                <div className="book-shelf-changer">
+                    <select>
+                        <option value="none" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
+                </div>
+            </div>
+            <div className="book-title">{title}</div>
+            <div className="book-authors">{authors}</div>
+        </div>
+    );
+}
 
 class BookShelves extends Component {
+    state = {
+        books: []
+    }
+    componentDidMount(){
+        BooksAPI.getAll().then((books => this.setState({books})))
+    }
     render() {
+        const wantToReadBooks = this.state.books.filter(x => x.shelf === 'wantToRead');
+        const currentlyReadingBooks = this.state.books.filter(x => x.shelf === 'currentlyReading');
+        const readBooks = this.state.books.filter(x => x.shelf === 'read');
+        
         return (
             <div className="list-books">
                 <div className="list-books-title">
@@ -10,82 +59,9 @@ class BookShelves extends Component {
                 </div>
                 <div className="list-books-content">
                     <div>
-                        <div className="bookshelf">
-                            <h2 className="bookshelf-title">Currently Reading</h2>
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-                                    <li>
-                                        <div className="book">
-                                            <div className="book-top">
-                                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                                                <div className="book-shelf-changer">
-                                                    <select>
-                                                        <option value="none" disabled>Move to...</option>
-                                                        <option value="currentlyReading">Currently Reading</option>
-                                                        <option value="wantToRead">Want to Read</option>
-                                                        <option value="read">Read</option>
-                                                        <option value="none">None</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div className="book-title">To Kill a Mockingbird</div>
-                                            <div className="book-authors">Harper Lee</div>
-                                        </div>
-                                    </li>
-                                </ol>
-                            </div>
-                        </div>
-                        <div className="bookshelf">
-                            <h2 className="bookshelf-title">Want to Read</h2>
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-                                    <li>
-                                        <div className="book">
-                                            <div className="book-top">
-                                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")' }}></div>
-                                                <div className="book-shelf-changer">
-                                                    <select>
-                                                        <option value="none" disabled>Move to...</option>
-                                                        <option value="currentlyReading">Currently Reading</option>
-                                                        <option value="wantToRead">Want to Read</option>
-                                                        <option value="read">Read</option>
-                                                        <option value="none">None</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div className="book-title">1776</div>
-                                            <div className="book-authors">David McCullough</div>
-                                        </div>
-                                    </li>
-                                    
-                                </ol>
-                            </div>
-                        </div>
-                        <div className="bookshelf">
-                            <h2 className="bookshelf-title">Read</h2>
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-                                    <li>
-                                        <div className="book">
-                                            <div className="book-top">
-                                                <div className="book-cover" style={{ width: 128, height: 174, backgroundImage: 'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api")' }}></div>
-                                                <div className="book-shelf-changer">
-                                                    <select>
-                                                        <option value="none" disabled>Move to...</option>
-                                                        <option value="currentlyReading">Currently Reading</option>
-                                                        <option value="wantToRead">Want to Read</option>
-                                                        <option value="read">Read</option>
-                                                        <option value="none">None</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div className="book-title">Oh, the Places You'll Go!</div>
-                                            <div className="book-authors">Seuss</div>
-                                        </div>
-                                    </li>
-                                </ol>
-                            </div>
-                        </div>
+                        <BookShelf name="Want to Read" books={wantToReadBooks} />
+                        <BookShelf name="Read" books={currentlyReadingBooks}/>
+                        <BookShelf name="Reading" books={readBooks}/>
                     </div>
                 </div>
                 <div className="open-search">
